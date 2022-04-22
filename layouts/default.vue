445983bfc3dd0de391a620cd84e22ca4ec2061cb
gagaman182 <gagaman182@gmail.comm>
2022-04-22 07:27:30 +0700

    main first

---
+++ layouts/default.vue
@@ -0,0 +1,126 @@
+<template>
+  <v-app dark>
+    <!-- <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer> -->
+
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+      src="https://images.pexels.com/photos/1037993/pexels-photo-1037993.jpeg"
+    >
+      <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn> -->
+      <!-- <v-btn icon @click.stop="clipped = !clipped">
+        <v-icon>mdi-application</v-icon>
+      </v-btn> -->
+      <!-- <v-btn icon @click.stop="fixed = !fixed">
+        <v-icon>mdi-minus</v-icon>
+      </v-btn> -->
+      <!-- <v-toolbar-title v-text="title" /> -->
+      <img src="@/assets/65.gif" />
+      <!-- <v-spacer /> -->
+      <h1 class="animate__animated animate__headShake white--text" dark>
+        ตารางเวรแพทย์โรงพยาบาลหาดใหญ่
+      </h1>
+      <!-- <v-list-item-title class="text-h2">
+        ตารางเวรแพทย์โรงพยาบาลหาดใหญ่
+      </v-list-item-title> -->
+
+      <!-- <v-spacer /> -->
+      <!-- <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+        <v-icon>mdi-menu</v-icon>
+      </v-btn> -->
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light> mdi-repeat </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import 'animate.css'
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      clipped: true,
+      drawer: true,
+      fixed: true,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        // {
+        //   icon: 'mdi-chart-bubble',
+        //   title: 'Inspire',
+        //   to: '/inspire',
+        // },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'ตารางเวรแพทย์',
+    }
+  },
+}
+</script>
+</script>
+<style>
+#app {
+  font-family: 'Trirong', serif;
+}
+img {
+  width: 4%;
+  height: auto;
+  display: block;
+  margin-right: 15px;
+}
+h1 {
+  font-size: 32px;
+  background: -webkit-linear-gradient(#eee, #333);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12">
+      <div class="justify-center mb-6">
+        <!-- <v-alert border="left" colored-border color="#7CB342" elevation="2">
+          <div
+            class="white-text pa-2 text-h4 animate__animated animate__headShake"
+            outlined
+            tile
+          >
+            {{ opddate_now }}
+          </div>
+        </v-alert> -->
+        <v-alert
+          color="#39AEA9"
+          dark
+          icon="mdi-clipboard-text-clock "
+          border="left"
+          prominent
+          elevation="2"
+          outlined
+          tile
+        >
+          <div
+            class="white-text pa-2 text-h5 animate__animated animate__headShake"
+            outlined
+            tile
+          >
+            {{ opddate_now }}
+          </div>
+        </v-alert>
+      </div>
+
+      <v-card>
+        <v-card-title class="headline"> </v-card-title>
+        <v-card-text>
+          <v-row>
+            <v-col cols="12" sm="4"><Morning_shift /></v-col>
+            <v-col cols="12" sm="4"><Afternoon_shift /></v-col>
+            <v-col cols="12" sm="4"><Night_shirt /></v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions>
+          <!-- <v-spacer /> -->
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import 'animate.css'
+import Morning_shift from '~/components/Morning_shift.vue'
+import Afternoon_shift from '~/components/Afternoon_shift.vue'
+import Night_shirt from '~/components/Night_shirt.vue'
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      opddate_now: new Date().toLocaleDateString('th-TH', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        weekday: 'long',
+      }),
+    }
+  },
+  components: {
+    Morning_shift,
+    Afternoon_shift,
+    Night_shirt,
+  },
+}
+</script>
+<style>
+#app {
+  font-family: 'Trirong', serif;
+}
+</style>
